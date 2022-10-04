d782b3ff13de66b431a00452cf473005979dd668
AlexBrown4020 <balex3497@gmail.com>
2022-10-04 16:10:06 +0900

    Change component pathing to source folder

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
 const useFetch = (url) => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 import useFetch from '../hooks/useFetch.js';
 
 export const Search = () => {
-    const {data, loading} = useFetch('localhost:8000')
-
+    const {data, loading} = useFetch('localhost:8000');
+    return (
+        <div>
+            <h1>Im search component</h1>
+            { data }
+        </div>
+    )
 }
