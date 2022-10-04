8065991ce00cf6f676d869feac9de2e89e511f5d
AlexBrown4020 <balex3497@gmail.com>
2022-10-04 16:00:23 +0900

    Feat: Implement first search feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
+import Search from '../routes/search';
 
 function App() {
   return (
     <BrowserRouter>
+      <Routes>
+        <Route path='/search' element={<Search/>}/>
+      </Routes>
       <h1>Hello World</h1>
     </BrowserRouter>
   );
