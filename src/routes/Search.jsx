b66d202e16a69329e9bde7d2c66db8df4e4bee8a
AlexBrown4020 <balex3497@gmail.com>
2022-10-05 14:20:01 +0900

    Feat: Seperate convention and character data

--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -5,17 +5,34 @@ import './search.css'
 
 export function Search () {
     const {data, loading} = useFetch('http://localhost:8000/search');
+    const characters = data.characters;
+    const conventions = data.conventions;
     return (
         <div>
             <div className='titleContainer'>
                 <h1>Cosplay Conventions</h1>
             </div>
-            <div className='contentContainer'>
+            <div className='conventionContainer'>
             { loading ? (
                 'Loading, please wait.'
             ) : (
                 <div className='searchList'>
-                    { data.map(result => {
+                    { conventions.map(result => {
+                        return <div className='search'>
+                            <h3>{result.title}</h3>
+                            <p>{result.snippet}</p>
+                            <a href={result.link}>{result.link}</a>
+                        </div>
+                    })}
+                </div>
+            )}
+            </div>
+            <div className='characterContainer'>
+            { loading ? (
+                'Loading, please wait.'
+            ) : (
+                <div className='searchList'>
+                    { conventions.map(result => {
                         return <div className='search'>
                             <h3>{result.title}</h3>
                             <p>{result.snippet}</p>
