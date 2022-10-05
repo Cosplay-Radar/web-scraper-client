ec36bce1617437e8ab79019792223253f8525e52
AlexBrown4020 <balex3497@gmail.com>
2022-10-05 10:07:44 +0900

    Feat: Display searched convention content

--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -3,11 +3,23 @@ import React from 'react';
 import useFetch from '../hooks/useFetch';
 
 export function Search () {
-    const {data, loading} = useFetch('http://localhost:8000');
+    const {data, loading} = useFetch('http://localhost:8000/search');
     return (
         <div>
             <h1>Im search component</h1>
-            { data }
+            { loading ? (
+                'Loading, please wait.'
+            ) : (
+                <div className='searchList'>
+                    { data.map(result => {
+                        return <div className='search'>
+                            <h3>{result.title}</h3>
+                            <p>{result.snippet}</p>
+                            <a href={result.link}>{result.link}</a>
+                        </div>
+                    })}
+                </div>
+            )}
         </div>
     )
 }
