e47298463ebb512ace594ca16c8be494956e8dad
AlexBrown4020 <balex3497@gmail.com>
2022-10-06 11:29:13 +0900

    Feat: Apply flex display to search results

--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -10,38 +10,41 @@ export function Search () {
     return (
         <div>
             <div className='titleContainer'>
-                <h1>Cosplay Conventions</h1>
+                <h1>Cosplay Market Research</h1>
             </div>
-            <div className='conventionContainer'>
-            { loading ? (
-                'Loading, please wait.'
-            ) : (
-                <div className='searchList'>
-                    { characters.map(result => {
-                        return <div className='search'>
-                            <h3>{result.title}</h3>
-                            <p>{result.snippet}</p>
-                            <a href={result.link}>{result.link}</a>
-                        </div>
-                    })}
+            <div className='resultsContainer'>
+                <div className='conventionContainer'>
+                { loading ? (
+                    'Loading, please wait.'
+                ) : (
+                    <div className='searchList'>
+                        { characters.map(result => {
+                            return <div className='search'>
+                                <h3>{result.title}</h3>
+                                <p>{result.snippet}</p>
+                                <a href={result.link}>{result.link}</a>
+                            </div>
+                        })}
+                    </div>
+                )}
                 </div>
-            )}
-            </div>
-            <div className='characterContainer'>
-            { loading ? (
-                'Loading, please wait.'
-            ) : (
-                <div className='searchList'>
-                    { conventions.map(result => {
-                        return <div className='search'>
-                            <h3>{result.title}</h3>
-                            <p>{result.snippet}</p>
-                            <a href={result.link}>{result.link}</a>
-                        </div>
-                    })}
+                <div className='characterContainer'>
+                { loading ? (
+                    'Loading, please wait.'
+                ) : (
+                    <div className='searchList'>
+                        { conventions.map(result => {
+                            return <div className='search'>
+                                <h3>{result.title}</h3>
+                                <p>{result.snippet}</p>
+                                <a href={result.link}>{result.link}</a>
+                            </div>
+                        })}
+                    </div>
+                )}
                 </div>
-            )}
             </div>
+
         </div>
     )
 }
