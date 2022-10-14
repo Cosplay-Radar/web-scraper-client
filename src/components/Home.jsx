3bb071345cb584febfb40a0c4891d6ee24cf22e5
AlexBrown4020 <balex3497@gmail.com>
2022-10-14 10:55:02 +0900

    Feat: Include link to organization github account

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,9 +11,16 @@ export function Home () {
                 <div className='content'>
                     <h3>About Cosplay Radar</h3>
                     <p>
-                        Cosplay Radar was built for Adventurer's Hoard; a 3D design/printing company that specializes in cosplay and miniature models.  
-                        The researcher app will be able to show you upcoming conventions and the current most popular characters.  Plan your product releases and what to research for the future!
+                        Cosplay Radar was built for Adventurer's Hoard: a 3D design/printing company that specializes in cosplay and miniature models.  
+                        This application will be able to show you upcoming conventions and the current most popular characters.  Plan your product releases and what to research for the future!
                         Try clicking on the links above to discover some useful information.
+                    </p>
+                    <p>
+                        If you would like to see the Github repository you can click on this link: <a href='https://github.com/Cosplay-Radar'>Cosplay Radar</a>
+                    </p>
+                    <p>
+                        You can locate the developers contact details, the store page, and social media at the bottom of the page.  Feel free to get in contact
+                        if you would like to contribute or have any suggestions for the project.
                     </p>
                 </div>
                     <h3>About the Developer:</h3>
