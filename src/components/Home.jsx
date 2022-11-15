515731aaaf39bb1dd51cd0f8c3abb341a04fdf5f
AlexBrown4020 <balex3497@gmail.com>
2022-11-15 10:13:08 +0900

    Feat: Home page footer at bottom of page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,7 @@ export function Home () {
                     </p>
                 </div>
             </div>
-            <Footer/>
+            <Footer id='homeFooter'/>
         </div>
     )
 }
