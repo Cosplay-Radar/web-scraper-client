05eecdcbddee99d6b265f959a0f0a701d46ae625
AlexBrown4020 <balex3497@gmail.com>
2022-12-12 11:24:14 +0900

    Feat: Fix routing for about section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Convention } from './routes/conventions/Convention.jsx';
 import { Character } from './routes/characters/Character.jsx';
-import { About } from './components/Home.jsx';
+import { About } from './routes/about/About.jsx';
 
 function App() {
   return (
