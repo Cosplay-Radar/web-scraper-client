1b654fd6aeb32b92dd17c31dc84ea502bf967382
AlexBrown4020 <balex3497@gmail.com>
2022-12-13 10:54:16 +0900

    Feat: Move log after data loaded

--- src/routes/conventions/Convention.jsx
+++ src/routes/conventions/Convention.jsx
@@ -6,22 +6,21 @@ import './convention.css'
 
 export function Convention () {
     const [page, setPage] = useState(1);
-    const [pageCount, setPageCount] = useState(0);
+    const [pageCount] = useState(0);
     const {data, loading} = useFetch(`https://cosplay-radar.herokuapp.com/conventions?page=${page}`);
-    console.log(data)
 
-    const detailsSplit = (string) => {
-        const output = {date:'', location: ''}
-        for (let i = string.length; i > 0; i--) {
-          if (!isNaN(parseInt(string[i]))) {
-            output.date = string.slice(0, i+1);
-            output.location = string.slice(i+1, string.length);
-            return output
-          }
-        }
-        console.log(setPageCount)
-        return output;
-      }
+    // const detailsSplit = (string) => {
+    //     const output = {date:'', location: ''}
+    //     for (let i = string.length; i > 0; i--) {
+    //       if (!isNaN(parseInt(string[i]))) {
+    //         output.date = string.slice(0, i+1);
+    //         output.location = string.slice(i+1, string.length);
+    //         return output
+    //       }
+    //     }
+    //     console.log(setPageCount)
+    //     return output;
+    //   }
 
       const handlePrevious = () => {
         setPage((p) => {
@@ -48,7 +47,10 @@ export function Convention () {
                         <p id='loadingText'>'Loading, please wait.'</p>
                     ) : (
                         <div className='searchList'>
-                            { data.cons.map(result => {
+                            {
+                                console.log(data)
+                            }
+                            {/* { data.cons.map(result => {
                                 if (result === undefined) return <div></div>;
                                 const output = detailsSplit(result.details)                              
                                     return <div className='search'>
@@ -60,7 +62,7 @@ export function Convention () {
                                             <p className='searchDetails'>{output.date}</p>
                                         </div>
                                     </div>
-                            })}
+                            })} */}
                         </div>
                     )}
                     </div>
