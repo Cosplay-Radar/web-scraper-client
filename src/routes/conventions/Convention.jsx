1cacf06719cc0bb827f6dbed6f40ff4aaabefc8d
AlexBrown4020 <balex3497@gmail.com>
2022-12-15 10:16:11 +0900

    Feat: Change all convention info alignment

--- src/routes/conventions/Convention.jsx
+++ src/routes/conventions/Convention.jsx
@@ -81,9 +81,11 @@ export function Convention () {
                 }
                     </div>
                 </div>
-                <div id='pageSelect'>
-                    <p>Page: {page}<br/></p>
-                    <p>Page Total:{data.pagination?.pageCount}</p>
+                <div id='pageNav'>
+                    <div id='pageSelect'>
+                        <p>Page: {page}<br/></p>
+                        <p>Page Total:{data.pagination?.pageCount}</p>
+                    </div>
                     <div id='buttonSelect'>
                         <button disabled={page === 1} onClick={handlePrevious}>Previous</button>
                         <button disabled={page >= pageCount} onClick={handleNext}>Next</button>
