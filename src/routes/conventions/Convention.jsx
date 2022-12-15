350189bc4872cc38f422a3b9df58f746b7973789
AlexBrown4020 <balex3497@gmail.com>
2022-12-15 10:22:40 +0900

    Feat: Show correct number of pages for remainder items

--- src/routes/conventions/Convention.jsx
+++ src/routes/conventions/Convention.jsx
@@ -16,7 +16,11 @@ export function Convention () {
             async function() {
                 try {
                     const response = await axios.get(`https://cosplay-radar.herokuapp.com/conventions?page=${page}`);
-                    setPageCount(response.pagination.pageCount);
+                    if (response.pagination.finalPage > 0) {
+                        setPageCount(response.pagination.pageCount + 1);
+                    } else {
+                        setPageCount(response.pagination.pageCount);
+                    }
                 } catch (error) {
                     console.log(error)
                 }
